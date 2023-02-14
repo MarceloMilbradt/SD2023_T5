b25e19b3fb79eb5cc081ef34fb6c85760f297780
gianriske <gian.riske@gmail.com>
2023-02-14 20:37:26 -0300

    t5.json

--- nodejs/script.js
+++ nodejs/script.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-axios.get('https://projetosd-d46bb-default-rtdb.firebaseio.com/interop.json')
+axios.get('https://projetosd-d46bb-default-rtdb.firebaseio.com/t5.json')
     .then(response => {
         console.log(response.data);
     })
